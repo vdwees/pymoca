vdwees/pymoca

commit b96ff6514023523ea8e0c9015120082466cf8ff3

Move pymola package to src subfolder
This is common practice to avoid e.g. accidentally running tests against
the source directory instead of the installed directory. See e.g.
https://hynek.me/articles/testing-packaging/ for more explanation.

setup.py
@@ -131,10 +131,8 @@ def setup_package():
         tests_require=['coverage >= 3.7.1', 'nose >= 1.3.1'],
         test_suite='nose.collector',
         python_requires='>=3.5',
-        packages=find_packages(
-            # choosing to distribute tests
-            # exclude=['*.test']
-        ),
+        packages=find_packages("src"),
+        package_dir={"": "src"},
         cmdclass={
             'antlr': AntlrBuildCommand,
             'versioneer': versioneer.get_cmdclass(),

commit dcd2ca49ba313c735dc58d69a91f0af96e2f7377

Fix some broken paths to src folder
Regression introduced in b96ff6514023523ea8e0c9015120082466cf8ff3

setup.py
@@ -80,13 +80,13 @@ def call_antlr4(arg):
     #pylint: disable=unused-argument, unused-variable
     antlr_path = os.path.join(ROOT_DIR, "java", "antlr-4.7-complete.jar")
     classpath = ".:{:s}:$CLASSPATH".format(antlr_path)
-    generated = os.path.join(ROOT_DIR, 'pymola', 'generated')
+    generated = os.path.join(ROOT_DIR, 'src', 'pymola', 'generated')
     cmd = "java -Xmx500M -cp \"{classpath:s}\" org.antlr.v4.Tool {arg:s}" \
             " -o {generated:s} -visitor -Dlanguage=Python3".format(**locals())
     print(cmd)
-    proc = subprocess.Popen(cmd.split(), cwd=os.path.join(ROOT_DIR, 'pymola'))
+    proc = subprocess.Popen(cmd.split(), cwd=os.path.join(ROOT_DIR, 'src', 'pymola'))
     proc.communicate()
-    with open(os.path.join(ROOT_DIR, 'pymola', 'generated', '__init__.py'), 'w') as fid:
+    with open(os.path.join(generated, '__init__.py'), 'w') as fid:
         fid.write('')
 
 def setup_package():